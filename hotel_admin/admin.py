e48b10db17aa332211d8a74a9e2f0546c92856b1
Ihor Dreyev <dre@syneforge.com>
2021-04-14 11:31:24 +0300

    table booking to admin

--- hotel_admin/admin.py
+++ hotel_admin/admin.py
@@ -18,5 +18,6 @@ admin.site.register(Room, admin.ModelAdmin)
 admin.site.register(Dish, admin.ModelAdmin)
 admin.site.register(Order, OrderAdmin)
 admin.site.register(RoomService, admin.ModelAdmin)
+admin.site.register(TableBooking, admin.ModelAdmin)
 admin.site.register(News, admin.ModelAdmin)
 
